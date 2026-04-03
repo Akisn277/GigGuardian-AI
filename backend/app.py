fe66b64a3ea75c9d53743b1e1d3382f7745d1dbf
Akshara Satheesh Nair <131741313+Akisn277@users.noreply.github.com>
2026-04-03 22:49:24 +0530

    deployment

--- backend/app.py
+++ backend/app.py
@@ -299,9 +299,6 @@ def monthly_stats():
 @app.route('/')
 def home():
     return send_from_directory('../', 'index.html')
-if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0',
-        port=int(os.getenv('PORT', '5000')),
-        debug=os.getenv('FLASK_DEBUG', 'false').lower() == 'true'
-    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
